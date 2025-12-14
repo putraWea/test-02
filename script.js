6c0acc275130d3e5d394e470e2e30f5354d21cf5
putraWea <putrawea02@gmail.com>
2025-12-14 18:59:41 +0700

    cobaa-04

---
+++ script.js
@@ -0,0 +1,29 @@
+// const button = document.getElementById("hapus");
+// const button2 = document.getElementById("change");
+// const box = document.getElementById("box");
+
+// button.addEventListener("click", () => {
+//   box.classList.remove("box2");
+// });
+
+// button2.addEventListener("click", () => {
+//   box.classList.add("box2");
+// });
+
+
+// // coba
+
+// const buttons = document.querySelectorAll(".button-test button");
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const color = btn.dataset.color;
+
+//     // hapus semua warna dulu
+//     box.classList.remove("red", "yellow", "green");
+
+//     // tambahkan warna sesuai tombol
+//     box.classList.add(color);
+//   });
+// });
+
